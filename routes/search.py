effbe00a179669267a4291ba8e07b402faca1e69
cirrostech <56857967+cirrostech@users.noreply.github.com>
2021-05-25 00:07:39 +0800

    Add files via upload

--- routes/search.py
+++ routes/search.py
@@ -15,10 +15,14 @@ headers = {
 }
 
 
-@search_blueprint.route("/",methods=['GET'],endpoint='index')
+@search_blueprint.route("/",methods=['GET', 'POST'],endpoint='index')
 def index():
 
-  return render_template("index.html")
+  if request.method == 'POST':
+    print("POST")
+    return render_template("index.html", search_term="Unlock")
+  elif request.method == 'GET':
+    return render_template("index.html")
 
 @search_blueprint.route("/search",methods=['GET','POST'],endpoint='search')
 def search():
@@ -31,20 +35,39 @@ def search():
   print("Search Term:{search_term}".format(search_term=search_term))
   url = "http://192.168.0.25:9200"
 
-  if request.method == 'POST':
-    _from = 0
-  elif request.method == 'GET':
-    _from = querystring
-
-
-  payload = {
-    "query": {
-      "query_string": {
+  base_query = {
+    "query_string": {
       "analyze_wildcard": True,
       "query": str(search_term),
-      "fields": ["issue", "company"]
-      }
-    },     
+			"fields": ["issue", "company"]
+    }
+  }
+
+  agg_term = "Checking account"
+  agg_query = {
+    "bool": {
+    	"must": [
+        base_query,
+        {
+        	"match": {
+            "sub_product": str(agg_term)
+    	 		}
+        }
+    	]
+    }
+	}
+
+  if request.method == 'POST':
+    _from = 0
+    str_query = base_query
+
+  elif request.method == 'GET':
+    _from = querystring
+    str_query = agg_query
+ 
+  payload = {
+    "query":
+     str_query , 
     "from": page_from,
     "size": 10,
     "sort": [],
